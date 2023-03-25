b51650dab23ec6da911d456d25ec000c43dfa4a5
Roman Kurnovskii <r.kurnovskii@gmail.com>
2023-03-25 21:42:55 +0300

    1809

---
+++ solutions/1809/A_Garland.py
@@ -0,0 +1,12 @@
+def solve(s):
+    if all(c == s[0] for c in s):
+        return -1
+    elif s.count(s[0]) == 3 or s.count(s[1]) == 3:
+        return 6
+    else:
+        return 4
+
+t = int(input())
+for _ in range(t):
+    s = input()
+    print(solve(s))
---
+++ solutions/1809/description.md
@@ -0,0 +1,3 @@
+[Educational Codeforces Round 145 (Rated for Div. 2)](https://codeforces.com/contest/1809)
+
+TODO: B+
