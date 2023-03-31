6fadaf1d5f902118a850c8e1fb3b47a586fe952f
Roman Kurnovskii <r.kurnovskii@gmail.com>
2023-03-31 12:33:26 +0300

    1809C

---
+++ solutions/1809/C_Sum_on_Subarrays.md
@@ -0,0 +1,14 @@
+You are given two numbers, 'n' and 'k'. Your task is to create an array of length 'n' with the sum of the elements equal to 'k'. The array can contain any integer numbers, but there is a condition - the sum of any contiguous subarray should be non-positive (less than or equal to 0).
+
+You need to find an array that satisfies the given condition.
+
+Example:
+
+Input:
+
+`n = 3, k = 5`
+
+Output:
+
+`[-1, 2, -6]`
+
---
+++ solutions/1809/C_Sum_on_Subarrays.py
@@ -0,0 +1,28 @@
+def solve(n, k):
+    res = [-1000] * n
+    if k == 0:
+        return ' '.join(map(str, res))
+
+    for i in range(n):
+        possible = (i+1) * (i+2) // 2
+        res[i] = 2
+
+        if possible == k:
+            break
+
+        if k - possible <= i+1:
+            if k - possible == i+1:
+                res[i+1] = -1
+            else:
+                x = 2 * (i+1) + 1
+                x -= 2 * (k - possible)
+                res[i+1] = -x
+
+            break
+
+    return ' '.join(map(str, res))
+
+
+for _ in range(int(input())):
+    n, k = list(map(int, input().split()))
+    print(solve(n, k))
