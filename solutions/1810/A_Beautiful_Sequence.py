0cdb2ed5c0e399e969fc084f2b2c75b13d5d26b7
Roman Kurnovskii <r.kurnovskii@gmail.com>
2023-04-01 08:30:28 +0300

    1810A

---
+++ solutions/1810/A_Beautiful_Sequence.py
@@ -0,0 +1,11 @@
+def solve():
+    n = int(input())
+    ar = list(map(int, input().split()))
+    for i in range(n):
+        if(ar[i] <= i + 1):
+            print("YES")
+            return
+    print("NO")
+            
+for _ in range(int(input())):
+    solve()
